39164106079496787082fb79656b919b4bb683cd
clementboloch <clement.boloch@gmail.com>
2024-01-12 18:55:14 +0100

    initializes the project

---
+++ src/CRM/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, unique=True)
+    phone = models.CharField(max_length=50)
+    company = models.CharField(max_length=50)
+    created_date = models.DateField(auto_now_add=True)
+    last_update = models.DateField(auto_now=True)
+    commercial_contact = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.email
+
+
+class Contract(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    amount = models.FloatField(max_length=50)
+    outstading_amount = models.FloatField(max_length=50)
+    created_date = models.DateTimeField(auto_now_add=True)
+    is_signed = models.BooleanField(default=False)
+
+
+class Event(models.Model):
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    location = models.CharField(max_length=50)
+    attendees = models.IntegerField()
+    notes = models.TextField(max_length=10000)
+    support_contact = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Department(models.Model):
+    COMMERCIAL = "C"
+    SUPPORT = "S"
+    GESTION = "G"
+    DEPARTMENT_CHOICES = [
+        (COMMERCIAL, "Commercial"),
+        (SUPPORT, "Support"),
+        (GESTION, "Gestion"),
+    ]
+    name = models.CharField(
+        max_length=1,
+        choices=DEPARTMENT_CHOICES,
+    )
+
