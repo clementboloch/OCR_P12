695c69ffb6ab64271fe10792c49ee2be981b7b1b
clementboloch <clement.boloch@gmail.com>
2024-01-19 12:12:31 +0100

    adds serializers

---
+++ src/CRM/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from models import *
+from django.shortcuts import get_object_or_404
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Client
+
+
+class ClientCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        exclude = ['created_date', 'last_update']
+        model = Client
+
+
+class ContractSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Contract
+
+
+class ContractCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        exclude = ['created_date']
+        model = Contract
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Event
